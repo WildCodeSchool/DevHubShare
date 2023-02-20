da14b98b35189a61cd7643229f76813b7f84c533
geoffont <geoffroy.font@gmail.com>
2023-02-20 10:12:23 +0100

    update of the controller to take into account the modifications

--- backend/src/models/UserHasLanguageManager.js
+++ backend/src/models/UserHasLanguageManager.js
@@ -15,9 +15,19 @@ class UserHasLanguageManager extends AbstractManager {
 
   update(user_has_language) {
     return this.database.query(
-      `update ${this.table} set user_id = ?, language_id = ?`,
-      [user_has_language.user_id, user_has_language.language_id]
+      `update ${this.table} set user_id = ?, language_id = ? where id = ?`,
+      [
+        user_has_language.user_id,
+        user_has_language.language_id,
+        user_has_language.id,
+      ]
     );
+  }
+
+  deleteAllByUserId(userId) {
+    return this.database.query(`DELETE FROM ${this.table} WHERE user_id = ?`, [
+      userId,
+    ]);
   }
 }
 
