f096d8a2c8ff86cd0b2d58c256f99263e34f14ea
BettyDetroit <caubelnelly@gmail.com>
2023-02-27 14:27:31 +0100

    Swipeable sidebar responsive work in progress

--- frontend/src/components/Sidebar/SwipeableSidebar.jsx
+++ frontend/src/components/Sidebar/SwipeableSidebar.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import "./swipeableSidebar.css";
+// import "./swipeableSidebar.css";
 import Box from "@mui/material/Box";
 import SwipeableDrawer from "@mui/material/SwipeableDrawer";
 import Button from "@mui/material/Button";
@@ -11,8 +11,25 @@ import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import Stack from "@mui/material/Stack";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Link } from "react-router-dom";
 import { SidebarData } from "./SidebarData";
+
+const drawerWidth = "15rem";
+
+const theme = createTheme({
+  typography: {
+    fontSize: 19,
+  },
+  spacing: 1,
+});
+
+const themeSelect = createTheme({
+  typography: {
+    fontSize: 14,
+  },
+});
 
 export default function SwipeableSidebar() {
   const [open, setOpen] = React.useState(false);
@@ -59,71 +76,131 @@ export default function SwipeableSidebar() {
       // onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
     >
-      <List className="sidebarList">
-        <ListItem disablePadding className="sidebarListItem">
-          <ListItemButton component={Link} to="/creer-post">
-            <ListItemText className="sidebarListItemText" primary="Accueil" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem
-          key="Fil de discussion"
-          disablePadding
-          className="sidebarListItem"
-        >
-          <ListItemButton onClick={handleOpen}>
-            <ListItemText
-              className="sidebarListItemText"
-              primary="Fil de discussion"
-            />
-          </ListItemButton>
-        </ListItem>
+      <Stack
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={4}
+      >
+        <ThemeProvider theme={theme}>
+          {/* <Toolbar /> */}
+          {/* <Box> */}
+          <List sx={{ mx: "auto" }}>
+            <ListItem disablePadding sx={{ mt: 70 }}>
+              <ListItemButton component={Link} to="/creer-post">
+                <ListItemText
+                  sx={{
+                    color: "#FFFFFF",
+                    textAlign: "center",
+                  }}
+                  primary="Accueil"
+                />
+              </ListItemButton>
+            </ListItem>
 
-        <FormControl className="sidebarFormControl">
-          <Select
-            className="sidebarSelect"
-            labelId="demo-controlled-open-select-label"
-            id="demo-controlled-open-select"
-            open={open}
-            onClose={handleClose}
-            onOpen={handleOpen}
-            displayEmpty
-            // eslint-disable-next-line prettier/prettier
-            renderValue={(value) => value || "Sélection du langage"}
-          >
-            {langages.map((langage) => (
-              <MenuItem
-                className="sidebarMenuItem"
-                key={langage}
-                component={Link}
-                to="/fil-de-discussion"
+            <ListItem key="Fil de discussion" disablePadding sx={{ mt: 50 }}>
+              <ListItemButton onClick={handleOpen}>
+                <ListItemText
+                  sx={{
+                    color: "#FFFFFF",
+                    textAlign: "center",
+                  }}
+                  primary="Fil de discussion"
+                />
+              </ListItemButton>
+            </ListItem>
+
+            <FormControl
+              sx={{
+                m: 1,
+                minWidth: 130,
+              }}
+            >
+              <Select
+                sx={{
+                  color: "#009AA6",
+                  height: 30,
+                  borderRadius: 2,
+                  fontSize: 14,
+                  backgroundColor: "white",
+                  pr: 10,
+                }}
+                labelId="demo-controlled-open-select-label"
+                id="demo-controlled-open-select"
+                open={open}
+                onClose={handleClose}
+                onOpen={handleOpen}
+                displayEmpty
+                // eslint-disable-next-line prettier/prettier
+                renderValue={(value) => value || "Sélection du langage"}
               >
-                {langage}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+                <ThemeProvider theme={themeSelect}>
+                  {langages.map((langage) => (
+                    <MenuItem
+                      sx={{
+                        color: "#009AA6",
+                      }}
+                      key={langage}
+                      component={Link}
+                      to="/fil-de-discussion"
+                    >
+                      {langage}
+                    </MenuItem>
+                  ))}
+                </ThemeProvider>
+              </Select>
+            </FormControl>
 
-        {SidebarData.map((item) => (
-          <ListItem key={item.title} disablePadding className="sidebarListItem">
-            <ListItemButton component={Link} to={item.path}>
-              <ListItemText primary={item.title} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+            {SidebarData.map((item) => (
+              <ListItem key={item.title} disablePadding sx={{ mt: 50 }}>
+                <ListItemButton component={Link} to={item.path}>
+                  <ListItemText
+                    sx={{
+                      color: "#FFFFFF",
+                      textAlign: "center",
+                    }}
+                    primary={item.title}
+                  />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          {/* </Box> */}
+        </ThemeProvider>
+      </Stack>
     </Box>
   );
 
   return (
     <div>
-      <Button onClick={toggleDrawer("left", true)}>
-        <ArrowForwardIosIcon />
-      </Button>
+      <Box sx={{ mt: "38%" }}>
+        <Button
+          onClick={toggleDrawer("left", true)}
+          sx={{
+            color: "#009AA6",
+          }}
+        >
+          <ArrowForwardIosIcon />
+        </Button>
+      </Box>
       <SwipeableDrawer
         anchor="left"
         open={state.left}
         onClose={toggleDrawer("left", false)}
         onOpen={toggleDrawer("left", true)}
+        // variant="permanent"
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: {
+            width: drawerWidth,
+            boxSizing: "border-box",
+            backgroundColor: "#009AA6",
+          },
+          [`& .MuiModal-backdrop`]: {
+            width: 0,
+          },
+        }}
       >
         {list("left")}
       </SwipeableDrawer>
