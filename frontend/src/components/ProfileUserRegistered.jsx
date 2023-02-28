5705af4d9ba3a3f8ef32cbe81f3cf6769c30f81d
geoffont <geoffroy.font@gmail.com>
2023-02-28 16:40:45 +0100

    create profile user registered component

--- frontend/src/components/ProfileUserRegistered.jsx
+++ frontend/src/components/ProfileUserRegistered.jsx
@@ -4,7 +4,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Grid, Card, CardContent, Button } from "@material-ui/core";
 import Informations from "./profilComponents/Informations";
 import UserImage from "./profilComponents/UserImage";
-import TexteLibre from "./profilComponents/TexteLibre";
+import UserTextArea from "./registeredProfile/UserTextArea";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -75,13 +75,24 @@ function UserProfile() {
           </Grid>
         </Grid>
         <Grid className={classes.gridCard} item xs={12} md={6}>
+          <Grid item xs={12} className={classes.valider}>
+            <Button variant="contained">Déconnecter</Button>
+          </Grid>
           <Card className={classes.card}>
             <CardContent>
               <div className={classes.texteLibreContainer}>
-                <TexteLibre />
+                <UserTextArea />
               </div>
             </CardContent>
           </Card>
+          <Grid item xs={12} className={classes.valider}>
+            <Button variant="text" href="/creation-compte">
+              Modifier le profile
+            </Button>
+          </Grid>
+          <Grid item xs={12} className={classes.valider}>
+            <Button variant="text">Supprimer votre compte</Button>
+          </Grid>
         </Grid>
       </Grid>
     </div>
