cb836e60a6b7b82ab49a5b24d3bbc7cb50bea28a
geoffont <geoffroy.font@gmail.com>
2023-03-02 10:31:59 +0100

    create SignUp page with components

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import Header from "../components/Headermain/Header";
+import SwipeableSidebar from "../components/Sidebar/SwipeableSidebar";
 import SignUp from "../components/SignUp";
+import Footer from "../components/Footer";
 
 function SignUpPage() {
   return (
-    <div>
-      <SignUp />
+    <div className="parent">
+      <div className="div1">
+        <Header />
+      </div>
+      <div className="div2">
+        <SwipeableSidebar />
+      </div>
+      <div className="div3">
+        <SignUp />
+      </div>
+      <div className="div4">
+        <Footer />
+      </div>
     </div>
   );
 }
