5eff48cd9f82e586a1299d0df60fe5f18c9376fe
geoffont <geoffroy.font@gmail.com>
2023-03-07 16:26:21 +0100

    integrate filter with select language

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,119 +1,3 @@
-// /* eslint-disable no-unused-vars */
-// /* eslint-disable camelcase */
-// /* eslint-disable import/no-extraneous-dependencies */
-// import React, { useEffect, useState } from "react";
-// import axios from "axios";
-// import Avatar from "@material-ui/core/Avatar";
-// import Button from "@material-ui/core/Button";
-// import CssBaseline from "@material-ui/core/CssBaseline";
-// import TextField from "@material-ui/core/TextField";
-// import Link from "@material-ui/core/Link";
-// import Grid from "@material-ui/core/Grid";
-// import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-// import Typography from "@material-ui/core/Typography";
-// import { makeStyles } from "@material-ui/core/styles";
-// import Container from "@material-ui/core/Container";
-
-// const useStyles = makeStyles((theme) => ({
-//   paper: {
-//     marginTop: theme.spacing(8),
-//     display: "flex",
-//     flexDirection: "column",
-//     alignItems: "center",
-//   },
-//   avatar: {
-//     margin: theme.spacing(1),
-//     backgroundColor: theme.palette.secondary.main,
-//   },
-//   form: {
-//     width: "100%", // Fix IE 11 issue.
-//     marginTop: theme.spacing(3),
-//   },
-//   submit: {
-//     margin: theme.spacing(3, 0, 2),
-//   },
-// }));
-
-// export default function SignUp(pseudo, email, password) {
-//   const [userData, setUserData] = useState([]);
-//   const classes = useStyles();
-
-//   useEffect(() => {
-//     axios.post("http://localhost:5000/users").then((response) => {
-//       setUserData(response.data);
-//     });
-//   }, [SubmitEvent]);
-
-//   return (
-//     <Container component="main" maxWidth="xs">
-//       <CssBaseline />
-//       <div className={classes.paper}>
-//         <Avatar className={classes.avatar}>
-//           <LockOutlinedIcon />
-//         </Avatar>
-//         <Typography component="h1" variant="h5">
-//           Inscription
-//         </Typography>
-//         <form className={classes.form} noValidate>
-//           <Grid container spacing={2}>
-//             <Grid item xs={12}>
-//               <TextField
-//                 variant="outlined"
-//                 required
-//                 fullWidth
-//                 name="pseudo"
-//                 label="pseudo"
-//                 type="string"
-//               />
-//             </Grid>
-//             <Grid item xs={12}>
-//               <TextField
-//                 variant="outlined"
-//                 required
-//                 fullWidth
-//                 id="email"
-//                 label="Adresse mail"
-//                 name="email"
-//                 autoComplete="email"
-//               />
-//             </Grid>
-//             <Grid item xs={12}>
-//               <TextField
-//                 variant="outlined"
-//                 required
-//                 fullWidth
-//                 name="password"
-//                 value={password}
-//                 label="Mot de passe"
-//                 type="password"
-//                 id="password"
-//                 autoComplete="current-password"
-//               />
-//             </Grid>
-//           </Grid>
-//           <Button
-//             type="submit"
-//             fullWidth
-//             variant="contained"
-//             color="primary"
-//             className={classes.submit}
-//           >
-//             Inscription
-//           </Button>
-//           <Grid container justifyContent="flex-end">
-//             <Grid item>
-//               <Link href="/connexion" variant="body2">
-//                 Déja inscrit ? Se connecter
-//               </Link>
-//             </Grid>
-//           </Grid>
-//         </form>
-//       </div>
-//     </Container>
-//   );
-// }
-/* eslint-disable no-unused-vars */
-/* eslint-disable camelcase */
 /* eslint-disable import/no-extraneous-dependencies */
 import React, { useEffect, useState } from "react";
 import axios from "axios";
@@ -127,6 +11,7 @@ import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+import LangSelectSignUp from "./LangSelectSignUp";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -153,9 +38,24 @@ export default function SignUp() {
   const [pseudo, setPseudo] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [language_id, setLanguage_id] = useState("");
+  const [languageId, setLanguageId] = useState([]);
   const [selectedLanguage, setSelectedLanguage] = useState("");
+  const [sideLanguages, setSideLanguages] = useState([]);
   const classes = useStyles();
+
+  const getLanguages = () => {
+    axios
+      .get("http://localhost:5000/languages")
+      .then((response) => response.data)
+      .then((data) => {
+        setSideLanguages(data);
+        console.info("langages api", data);
+      });
+  };
+
+  useEffect(() => {
+    getLanguages();
+  }, []);
 
   useEffect(() => {
     axios.get("http://localhost:5000/users").then((response) => {
@@ -163,24 +63,39 @@ export default function SignUp() {
     });
   }, []);
 
+  const handleChange = (e) => {
+    console.info(e.target.value, "targetvalue");
+    setSelectedLanguage(e.target.value);
+  };
+
+  const selectedLanguageObj = sideLanguages.find(
+    (language) => language.language_name === selectedLanguage
+  );
+  const languagesId = selectedLanguageObj ? selectedLanguageObj.id : null;
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const newUser = {
       pseudo,
       email,
       password,
-      language_id: [parseInt(selectedLanguage, 10)],
+      language_id: [languagesId],
     };
+
+    console.info(selectedLanguage, "languageselected");
+    console.info(languagesId, "match ou pas");
+    console.info(newUser, "newuser");
+
     axios.post("http://localhost:5000/users", newUser).then((response) => {
       setUserData([...userData, response.data]);
       setPseudo("");
       setEmail("");
       setPassword("");
-      setLanguage_id("");
+      setLanguageId([selectedLanguage]);
       setSelectedLanguage("");
     });
   };
-
+  console.info(languageId, "usestate languageId");
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
@@ -232,18 +147,8 @@ export default function SignUp() {
                 onChange={(e) => setPassword(e.target.value)}
               />
             </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="language"
-                label="Language select"
-                name="language"
-                autoComplete="language"
-                value={language_id}
-                onChange={(e) => setLanguage_id(parseInt(e.target.value, 10))}
-              />
+            <Grid item xs={3}>
+              <LangSelectSignUp onChange={(e) => handleChange(e)} />
             </Grid>
           </Grid>
           <Button
