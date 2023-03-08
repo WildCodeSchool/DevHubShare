2f425c43e0dd6f69e1baa425ddab491c49944c08
geoffont <geoffroy.font@gmail.com>
2023-03-08 16:56:52 +0100

    create update API

--- frontend/src/components/profilComponents/Informations.jsx
+++ frontend/src/components/profilComponents/Informations.jsx
@@ -27,28 +27,57 @@ const useStyles = makeStyles((theme) => ({
 function Informations() {
   const [user, setUser] = useState("");
   const [userLanguages, setUserLanguages] = useState([]);
+  const [userUpdate, setUserUpdate] = useState("");
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [workplace, setWorkplace] = useState("");
+  const [github, setGithub] = useState("");
+  const [linkedin, setLinkedin] = useState("");
 
   const classes = useStyles();
   useEffect(() => {
-    axios.get("http://localhost:5000/users/208").then((response) => {
+    axios.get("http://localhost:5000/users/209").then((response) => {
       setUser(response.data);
     });
   }, []);
-
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const updateUser = {
+      firstname,
+      lastname,
+      workplace,
+      github,
+      linkedin,
+    };
+    useEffect(() => {
+      axios
+        .put("http://localhost:5000/users/209", updateUser)
+        .then((response) => {
+          setUserUpdate(response.data);
+          setFirstname("");
+          setLastname("");
+          setWorkplace("");
+          setGithub("");
+          setLinkedin("");
+        });
+    }, []);
+  };
   useEffect(() => {
-    axios
-      .get("http://localhost:5000/user_has_language")
-      .then((response) => response.data)
-      .then((data) => {
-        const filteredData = data.filter((lang) => lang.user_id === user.id);
-        const userLanguageObjects = filteredData.map((lang) => ({
-          id: lang.language_id,
-          language_name: lang.language_name,
-        }));
-        setUserLanguages(userLanguageObjects);
-        console.info("user languages", userLanguageObjects);
-      });
-  }, [user.id]);
+    if (user && user.id) {
+      // Vérifie si l'utilisateur est défini et a un ID avant de continuer
+      axios
+        .get(`http://localhost:5000/user_has_language?user_id=${user.id}`)
+        .then((response) => response.data)
+        .then((data) => {
+          const userLanguageObjects = data.map((lang) => ({
+            id: lang.language_id,
+            language_name: lang.language_name,
+          }));
+          setUserLanguages(userLanguageObjects);
+          console.info("user languages", userLanguageObjects);
+        });
+    }
+  }, [user]);
 
   return (
     <Paper className={classes.root}>
@@ -58,7 +87,12 @@ function Informations() {
       <div className={classes.userImageContainer}>
         <UserImage size="5rem" backgroundColor="grey" />
       </div>
-      <form className={classes.root} noValidate autoComplete="off">
+      <form
+        className={classes.root}
+        noValidate
+        autoComplete="off"
+        onSubmit={handleSubmit}
+      >
         <Grid container spacing={2}>
           <Grid item xs={12}>
             <TextField
@@ -79,6 +113,7 @@ function Informations() {
               label="Prénom"
               value={user.firstname}
               fullWidth
+              onChange={(e) => setFirstname(e.target.value)}
               InputLabelProps={{
                 shrink: true,
                 disableAnimation: true,
@@ -92,6 +127,7 @@ function Informations() {
               label="Nom"
               value={user.lastname}
               fullWidth
+              onChange={(e) => setLastname(e.target.value)}
               InputLabelProps={{
                 shrink: true,
                 disableAnimation: true,
@@ -118,6 +154,7 @@ function Informations() {
               label="Poste Actuel"
               value={user.workplace}
               fullWidth
+              onChange={(e) => setWorkplace(e.target.value)}
               InputLabelProps={{
                 shrink: true,
                 disableAnimation: true,
@@ -131,6 +168,7 @@ function Informations() {
               label="Git-Hub Page"
               value={user.github}
               fullWidth
+              onChange={(e) => setGithub(e.target.value)}
               InputLabelProps={{
                 shrink: true,
                 disableAnimation: true,
@@ -144,6 +182,7 @@ function Informations() {
               label="Linkedin"
               value={user.linkedin}
               fullWidth
+              onChange={(e) => setLinkedin(e.target.value)}
               InputLabelProps={{
                 shrink: true,
                 disableAnimation: true,
