37ccb2fd4c62c5d6775ff6916f7da70225a7be62
BettyDetroit <caubelnelly@gmail.com>
2023-03-09 17:29:22 +0100

    Sidebar hopefully finished

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,66 +1,79 @@
 import * as React from "react";
 import axios from "axios";
 import { useState, useEffect, useContext } from "react";
-import { useTheme, useMediaQuery } from "@mui/material";
-import FormControl from "@mui/material/FormControl";
-import MenuItem from "@mui/material/MenuItem";
-import Select from "@mui/material/Select";
+import { useMediaQuery, createTheme } from "@mui/material";
 import { Icon } from "@iconify/react";
 import { NavLink, Link } from "react-router-dom";
-import { SidebarData } from "./SidebarData";
 import SelectedLanguageContext from "../../services/context/SelectedLanguageContext";
+
+const themeMedia = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 800,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+});
 
 export default function Sidebar() {
   const { selectedLanguage, setSelectedLanguage } = useContext(
     SelectedLanguageContext
   );
-  const [selectOpen, setSelectOpen] = useState(false);
-  const theme = useTheme();
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const isMediumScreen = useMediaQuery(themeMedia.breakpoints.down("md"));
   const [showSidebar, setShowSidebar] = useState(true);
   const [showButton, setShowButton] = useState(false);
 
   const [sideLanguages, setSideLanguages] = useState([]);
 
-  const handleLanguageChange = (event) => {
-    setSelectedLanguage(event.target.value);
-  };
-  setSelectedLanguage(selectedLanguage);
+  function useLoggedIn() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const getLanguages = () => {
-    axios
-      .get("http://localhost:5000/languages")
-      .then((response) => response.data)
-      .then((data) => {
-        setSideLanguages(data);
-      });
-  };
+    // localStorage.setItem("user.id", "1");
+    useEffect(() => {
+      const userId = localStorage.getItem("user.id");
+      setIsLoggedIn(!!userId);
+    }, []);
 
+    return isLoggedIn;
+  }
+
+  const isLoggedIn = useLoggedIn();
+
+  const getLanguages = async () => {
+    try {
+      const { data } = await axios.get("http://localhost:5000/languages");
+      setSideLanguages(data);
+      // console.info("langages api", data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   useEffect(() => {
     getLanguages();
   }, []);
 
   useEffect(() => {
-    if (isSmallScreen) {
+    if (isMediumScreen) {
       setShowButton(true);
       setShowSidebar(false);
     } else {
       setShowButton(false);
       setShowSidebar(true);
     }
-  }, [isSmallScreen]);
+  }, [isMediumScreen]);
 
   const clickButton = () => {
     setShowSidebar(!showSidebar);
   };
 
-  const handleClose = () => {
-    setSelectOpen(false);
+  const handleLanguageChange = (event) => {
+    setSelectedLanguage(event.target.value);
   };
-
-  const handleOpen = () => {
-    setSelectOpen(true);
-  };
+  setSelectedLanguage(selectedLanguage);
+  console.info(selectedLanguage, "langage sélectionné");
 
   return (
     <div
@@ -88,78 +101,98 @@ export default function Sidebar() {
           }}
         >
           <NavLink
-            to="/"
+            to="/creer-post"
+            className="link"
+            style={{
+              display: "flex",
+              color: isLoggedIn ? "#ffff" : "rgb(120 111 111)",
+              padding: "1rem 2rem",
+              textDecoration: "none",
+            }}
+          >
+            <div className="link_text">Accueil</div>
+          </NavLink>
+          <div
+            className="sideFil"
+            style={{
+              textAlign: "center",
+            }}
+          >
+            <NavLink
+              to="/fil-de-discussion"
+              onClick={() => {
+                setSelectedLanguage(undefined);
+              }}
+              className="link"
+              style={{
+                display: "flex",
+                color: isLoggedIn ? "#ffff" : "rgb(120 111 111)",
+                padding: "1rem 2rem",
+                textDecoration: "none",
+                fontSize: "0.9rem",
+              }}
+            >
+              <div className="link_text">Fil de discussion</div>
+            </NavLink>
+            <select
+              value={selectedLanguage}
+              onChange={handleLanguageChange}
+              style={{
+                color: isLoggedIn ? "#009AA6" : "rgb(120 111 111)",
+                height: 30,
+                borderRadius: 4,
+                fontSize: 14,
+                backgroundColor: "white",
+              }}
+            >
+              <option value="">Sélection du langage</option>
+              {sideLanguages.map((langage) => (
+                <option
+                  key={langage.id}
+                  value={langage.langage_name}
+                  style={{ color: "#009AA6" }}
+                >
+                  <Link to="/fil-de-discussion"> {langage.language_name}</Link>
+                </option>
+              ))}
+            </select>
+          </div>
+          <NavLink
+            to="/profil-membre"
+            className="link"
+            style={{
+              display: "flex",
+              color: isLoggedIn ? "#ffff" : "rgb(120 111 111)",
+              padding: "1rem 2rem",
+              textDecoration: "none",
+            }}
+          >
+            <div className="link_text">Membres</div>
+          </NavLink>
+          <NavLink
+            to="/ressources"
             className="link"
             style={{
               display: "flex",
               color: "#ffff",
               padding: "1rem 2rem",
-              transition: "all 0.5s",
               textDecoration: "none",
-              "&:hover": {
-                textDecoration: "underline",
-              },
             }}
           >
-            <div className="link_text">Accueil</div>
+            <div className="link_text">Ressources</div>
           </NavLink>
-          <FormControl
-            sx={{
-              m: 1,
-              minWidth: 130,
+          <NavLink
+            to="/mes-posts"
+            className="link"
+            style={{
+              display: "flex",
+              color: isLoggedIn ? "#ffff" : "rgb(120 111 111)",
+              padding: "1rem 2rem",
+              textDecoration: "none",
             }}
           >
-            <Select
-              sx={{
-                color: "#009AA6",
-                height: 30,
-                borderRadius: 1,
-                fontSize: 14,
-                backgroundColor: "white",
-              }}
-              onChange={handleLanguageChange}
-              labelId="demo-controlled-open-select-label"
-              id="demo-controlled-open-select"
-              open={selectOpen}
-              onClose={handleClose}
-              onOpen={handleOpen}
-              displayEmpty
-              // eslint-disable-next-line prettier/prettier
-              renderValue={(value) => value || "Sélection du langage"}
-            >
-              {sideLanguages.map((langage) => (
-                <MenuItem
-                  sx={{
-                    color: "#009AA6",
-                  }}
-                  key={langage.id}
-                  value={langage.language_name}
-                  component={Link}
-                  to="/fil-de-discussion"
-                >
-                  {langage.language_name}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          {SidebarData.map((item) => {
-            return (
-              <NavLink
-                to={item.path}
-                key={item.kName}
-                className="link"
-                style={{
-                  display: "flex",
-                  color: "#ffff",
-                  padding: "1rem 2rem",
-                  transition: "all 0.5s",
-                  textDecoration: "none",
-                }}
-              >
-                <div className="link_text">{item.title}</div>
-              </NavLink>
-            );
-          })}
+            <div className="link_text">Mes Posts</div>
+          </NavLink>
         </div>
       </div>
       <div
