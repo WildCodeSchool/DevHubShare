b56d8326671b2d9b9e8b5b2ef1dbf428e3d01bfe
BettyDetroit <caubelnelly@gmail.com>
2023-04-13 19:20:08 +0200

    token validity is checked in privateRoutes

--- frontend/src/services/PrivateRoutes.jsx
+++ frontend/src/services/PrivateRoutes.jsx
@@ -1,6 +1,18 @@
 import { Outlet, Navigate } from "react-router-dom";
+import jwtDecode from "jwt-decode";
 
 export default function PrivateRoutes() {
   const token = localStorage.getItem("token");
-  return token ? <Outlet /> : <Navigate to="/inscription" />;
+
+  const isTokenValid = (t) => {
+    if (t) {
+      const { exp: expiration } = jwtDecode(t);
+      if (expiration * 1000 > new Date().getTime()) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  return isTokenValid(token) ? <Outlet /> : <Navigate to="/inscription" />;
 }
